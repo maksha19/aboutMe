aef8be642150e67fe69096e01e3bc93f30a88807
manikandan <maksha@outlook.com>
2021-06-25 23:26:33 +0800

    feat: init bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Card, Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container className="center-screen">
+        <Card style={{ width: "18rem", backgroundColor: "#17a2b8" }}>
+          <Card.Body>
+            <Card.Title className="text-center">Hello World</Card.Title>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
