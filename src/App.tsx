7c10806b0b953d977760f159ada36af4e609233c
manikandan <maksha@outlook.com>
2021-07-03 10:48:08 +0800

    test: revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Button } from "react-bootstrap";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Container className="center-screen">
         <Card style={{ width: "18rem", backgroundColor: "#17a2b8" }}>
           <Card.Body>
-            <Card.Title className="text-center">Hello World</Card.Title>
+            <Card.Title className="text-center">about Me</Card.Title>
           </Card.Body>
         </Card>
       </Container>
