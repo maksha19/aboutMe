a4a8dc565927bde37883300391429fa102f21734
manikandan <maksha@outlook.com>
2021-07-24 17:55:38 +0800

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
 import React from "react";
-import logo from "./random.jpg";
 import "./App.css";
-import { Card, Container, Button } from "react-bootstrap";
-//const logo = require("../public/logo512.png");
+import { Card, Container } from "react-bootstrap";
 
 function App() {
   return (
     <div>
       <Container className="center-screen">
-        <Card style={{ width: "18rem", backgroundColor: "#17a2b8" }}>
-          <Card.Body>
-            <Card.Title className="text-center">about Me</Card.Title>
-          </Card.Body>
-        </Card>
-        <div style={{ height: "100px", width: "100px" }}>
-          <img src={logo} alt="logo" />
+        <div style={{ width: "90vw", marginTop: "5vh" }}>
+          <Card
+            style={{
+              backgroundColor: "#1793bb",
+              height: "100%",
+            }}
+          >
+            <Card.Body>
+              <div style={{ height: "20%" }}>
+                <div className="text-end">Email: test@example.com</div>
+                <div className="text-end">phone: +65-1111 1111</div>
+              </div>
+              <div style={{ height: "70%" }} className="row">
+                <div className="col-sm-10 col-md-3 col-ls-5  text-center">
+                  <div className="profile-circle "></div>
+                  <div style={{ height: "10%" }} className="">
+                    Manikandan Shanmugam
+                  </div>
+                </div>
+                <div className=" align-self-center col-sm-9 col-md-9 col-ls-9">
+                  <div className="profile-text  text-wrap">
+                    about me
+                    njknjsndcjdncjdfnjfknnnnnnnnnncscnsjdncjsdncshjnchsdcnhsdx
+                    sc sdkc ds cd cd csdnjcknskjdc xsjkc sjkc
+                    sdjkcnxajsnxjasnxhsnhjcsnhcn
+                    xmsxmksjnxcjsncehdhwynjasxmaixanxhanxhjbbdwdqwndhjwbhxbhc
+                  </div>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
         </div>
       </Container>
+      <div className="d-flex bd-highlight">
+        <div className="p-2 flex-grow-1 bd-highlight">Flex item</div>
+        <div className="p-2 bd-highlight">Flex item</div>
+        <div className="p-2 bd-highlight">Third flex item</div>
+      </div>
     </div>
   );
 }
