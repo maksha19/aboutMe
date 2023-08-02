4b48ed57b5f3811bcb3d3fcdc212fc7388f0cec8
shanmugam_manikandan <shanmugam.manikandan@cognizant.com>
2023-08-02 08:08:13 +0800

    profile card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import profile from './profile.jpg'
 
 function App() {
   return (
@@ -43,17 +44,24 @@ function App() {
           <div className='w-full sm:w-2/5 h-full flex bg-[#e6dace]' ></div>
           <div className='w-0 sm:w-3/5  h-full' ></div>
         </div>
-        <div className='w-full absolute top-1/4 grid sm:grid-cols-12 grid-cols-8 gap-4'  >
+        <div className='w-full absolute top-16 sm:top-1/4 grid sm:grid-cols-12 grid-cols-8 gap-4'  >
           <div className='sm:col-start-4 sm:col-span-3 col-start-2 col-span-6'>
             <div className='w-full'>
               <div className="max-w-xs bg-[#f4ece6] shadow-md rounded-lg overflow-hidden">
-                <img className="object-cover h-48 w-full" src="image.jpg" alt="Card Image" />
-                <div className="p-4">
-                  <h3 className="text-xl font-medium text-gray-800 mb-2">Card Title</h3>
-                  <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.</p>
-                  <div className="mt-4">
-                    <a href="#" className="text-blue-500 font-medium">Read More</a>
-                  </div>
+                <div className='my-6'>
+                  <img className="w-48 h-48 rounded-full mx-auto" src={profile} alt="Card Image" />
+                </div>
+                <div className="grid justify-items-center mb-5">
+                  <div className='text-3xl font-extrabold font-sans text-gray-800 tracking-wide'>Manikandan</div>
+                  <div className='text-3xl font-extrabold font-sans text-gray-800 tracking-wide'>Shanmugam</div>
+                </div>
+                <div className="grid justify-items-center mb-5">
+                  <div className=" flex flex-grow border-t h-1 w-20 border-red-400"></div>
+                </div>
+                <div className="grid justify-items-center mb-6">
+                  <p className="text-gray-600 tracking-widest font-semibold uppercase">Senior Software Engineer</p>
+                </div>
+                <div className='h-20 bg-white'>
                 </div>
               </div>
             </div>
