49d192015e27b3016fc007eca07090d8df9d9f6e
shanmugam_manikandan <shanmugam.manikandan@cognizant.com>
2023-08-09 09:11:38 +0800

    list of projects

--- src/subPage/projectPage.tsx
+++ src/subPage/projectPage.tsx
@@ -4,8 +4,53 @@ import profile from '../profile.jpg'
 
 const ProjectPage = () => {
 
+    const projects = [
+        {
+            projectName: 'Video Analytics System (VAS)',
+            describe: "video analysis project",
+            link: 'https://www.developer.tech.gov.sg/products/categories/analytics/vas/overview.html'
+        },
+        {
+            projectName: 'GovWallet',
+            describe: "e-wallet project",
+            link: "https://www.wallet.gov.sg/"
+        },
+        {
+            projectName: 'supplyally',
+            describe: "distribution item project",
+            link: 'https://www.supplyally.gov.sg/'
+        }
+    ]
 
+    const listOfProject = () => {
 
+        return projects.map((project) => {
+            return (
+                <div className="grid grid-flow-row grid-cols-9 my-10">
+                    <div className="h-auto col-start-2 col-span-7 sm:col-start-3 sm:col-span-5">
+                        <div className="flex h-full flex-col bg-white  p-4  leading-normal">
+                            <div className="mt-8 text-center">
+                                <div className="text-gray-900 font-bold text-2xl mb-2 underline underline-offset-8">{project.projectName}</div>
+                            </div>
+                            <div className="p-8">
+                                <p className="text-gray-700 text-base">{project.describe}</p>
+                            </div>
+                            {project.link &&
+                                (
+                                    <div className='text-end'>
+                                        <a className="cta-base text-blue-700 hover:text-primary-700 active:text-primary-700 flex justify-end mr-24" href={project.link}>
+                                            Project link
+                                        </a>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+
+    }
 
 
     return (
@@ -20,24 +65,7 @@ const ProjectPage = () => {
                     </div>
 
                 </div>
-                <div className="grid grid-flow-row grid-cols-8">
-                    <div className="h-48  col-start-2 col-span-4">
-                        <div className="flex h-full flex-col bg-white  p-4  leading-normal">
-                            <div className="mt-8">
-                                <div className="text-gray-900 font-bold text-xl mb-2">Project Name</div>
-                            </div>
-                            <div className="mt-8">
-                                <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='h-48 col-start-6 col-span-1'>
-                    <div className="h-48  lg:w-48 flex-none bg-cover text-center overflow-hidden" title="Woman holding a mug">
-                        <img src={profile} alt="Card" />
-                    </div>
-                    </div>
-
-                </div>
+                {listOfProject()}
 
             </div>
         </div>
